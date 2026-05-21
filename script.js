3753c43a55cbd1227a7a0665ef565c77e28c6a6b
ctrl-aeon <fayemiolanrewaju7@gmail.com>
2026-05-21 06:53:56 +0100

    Add logic to resize, reset and paint!

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('.container');
-const containerWidth = container.getBoundingClientRect().width;
-const containerHeight = container.getBoundingClientRect().height;
+
 let reset = document.querySelector('.reset');
 let resize = document.querySelector('.resize');
 
@@ -11,24 +10,60 @@ function defaultGrid() {
     resizeGrid(16);
 }
 
-function resetGrid() {
-    resizeGrid(squarePerGrid)
-}
-
 function resizeGrid(squarePerGrid) {
     container.innerHTML = '';
     if (squarePerGrid === undefined) {
         squarePerGrid = Number(prompt('Enter the number of squares per side for the new grid.', 16));
     }
-    
+    const fragment = document.createDocumentFragment();
     for (i = 0; i < squarePerGrid ** 2; i++) {
-        cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.width = `${containerWidth / squarePerGrid}px`;
-        cell.style.height = `${containerHeight / squarePerGrid}px`;
 
-        container.append(cell);
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+
+        cell.style.width = `${100 / squarePerGrid}%`;
+        cell.style.height = `${100 / squarePerGrid}%`;
+
+// hover 
+        // cell.addEventListener('mouseenter', () => cell.style.backgroundColor = getRandomColor() );
+
+        let isDrawing = false;
+
+        document.addEventListener('mousedown', () => isDrawing = true);
+        document.addEventListener('mouseup', () => isDrawing = false);
+
+        cell.addEventListener('mouseenter', (e) => {
+            if (isDrawing) {
+                e.target.style.backgroundColor = getRandomColor();
+            }
+        });
+
+        fragment.append(cell);
     }
+    container.append(fragment);
+
+// // bubbling
+//     container.addEventListener('mouseover', (e) => {
+//             if (e.target.classList.contains('cell') ) {
+//                 e.target.style.backgroundColor = getRandomColor();
+//             }
+//         })
 }
 
-defaultGrid();
+defaultGrid()
+
+function resetGrid() {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+}
+
+
+function getRandomColor() {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
